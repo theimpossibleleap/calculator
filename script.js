fab4d29c16861d8ccbc1285a0ccf5afcafcab814
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-03-13 14:26:54 -0700

    Added boilerplate

---
+++ script.js
@@ -0,0 +1,9 @@
+arg1 = prompt("Choose your first number.")
+arg2 = prompt("Choose your second number.")
+
+function add(arg1, arg2) {
+    addResult = arg1 + arg2;
+    return addResult;
+}
+
+console.log(addResult);
