d5193684bd23115ebedb67cfe198b51961f867f4
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-03-13 14:54:55 -0700

    Pushing updates for the day

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 let numbers = [];
-let addResult = 0;
 
-numbers.push(Number(prompt("Please choose your first number.")));
-numbers.push(Number(prompt("Please choose your second number.")));
+
+
+let numOne = document.querySelector("#numOne").value;
+let numTwo = document.querySelector("#numTwo").value;
+
+numOne.push(numbers);
+numTwo.push(numbers);
+
+/* numbers.push(Number(prompt("Please choose your first number.")));
+numbers.push(Number(prompt("Please choose your second number."))); */
 
 function add() {
     addResult = numbers[0] + numbers[1];
     return addResult;
 }
 
-add();
+function subtract() {
+    subResult = numbers[0] - numbers[1];
+    return subResult;
+}
 
-console.log(addResult);
+function multiply() {
+    multResult = numbers[0] * numbers[1];
+    return multResult;
+}
+
+function divide() {
+    divideResult = numbers[0] / numbers[1];
+    return divideResult;
+}
+
+console.log(`
+Addition: ${numbers[0]} + ${numbers[1]} = ${add()}
+Subtraction: ${numbers[0]} - ${numbers[1]} = ${subtract()}
+Multiplication: ${numbers[0]} x ${numbers[1]} = ${multiply()}
+Division: ${numbers[0]} / ${numbers[1]} = ${divide()}
+`);
+
