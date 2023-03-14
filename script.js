15682e366b01b33ce773b56813a443f94a2eab0c
theimpossibleleap <122632859+theimpossibleleap@users.noreply.github.com>
2023-03-14 09:45:49 -0700

    Updated files.

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let numbers = [];
 
-/* numbers.push(Number(prompt("Please choose your first number.")));
-numbers.push(Number(prompt("Please choose your second number."))); */
+numbers.push(Number(prompt("Please choose your first number.")));
+numbers.push(Number(prompt("Please choose your second number.")));
 
 function add() {
     addResult = numbers[0] + numbers[1];
@@ -29,20 +29,3 @@ Subtraction: ${numbers[0]} - ${numbers[1]} = ${subtract()}
 Multiplication: ${numbers[0]} x ${numbers[1]} = ${multiply()}
 Division: ${numbers[0]} / ${numbers[1]} = ${divide()}
 `);
-
-const numDivs = 20;
-const container = document.getElementById("container");
-
-for (i = 0; i <= numDivs; i++) {
-    let div = document.createElement("div");
-    
-    div.setAttribute("id", "calcDiv"+i);
-    div.style.height = "50px";
-    div.style.width = "50px";
-    div.style.backgroundColor = "black";
-    div.style.borderColor = "white";
-    div.style.margin = '2px';
-    div.style.color = 'white';
-
-    container.appendChild(div);
-};
